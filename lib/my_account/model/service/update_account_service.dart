3567b9193581fcdd8ac9974ae6a9661f836af434
Maged Refaat <magedrefaatm@yahoo.com>
2021-05-23 13:17:00 +0200

    Latest Updates

--- lib/my_account/model/service/update_account_service.dart
+++ lib/my_account/model/service/update_account_service.dart
@@ -2,6 +2,7 @@ import 'package:store_go/verification/model/entities/account_info_response.dart'
 import 'package:store_go/my_account/model/entities/update_account_response.dart';
 import 'package:store_go/my_account/model/data/my_account_local_data.dart';
 import 'package:http/http.dart';
+import 'dart:convert';
 import 'dart:async';
 
 class UpdateAccountService {
@@ -23,29 +24,16 @@ class UpdateAccountService {
       List<Document> bankDocuments) async {
     UpdateAccountResponse updateResponse;
 
-    print(<String, dynamic>{
-      'application-id': applicationId,
-      'application-secret': applicationSecret,
-      'type': type,
-      'iban_number': ibanNumber,
-      'beneficiary_name': beneficiaryName,
-      'license_type': licenseType,
-      'commercial_registry_expiry_date': expireDate,
-      'business_name_en': englishName,
-      'business_name_ar': arabicName,
-      'business_address': address,
-      'business_mobile': mobileNumber.toString(),
-      'bank_id': bankId.toString(),
-      'business_documents': businessDocuments.toString(),
-      'bank_documents': bankDocuments.toString()
-    });
-
     try {
       final response = await post(
         Uri.parse(updateAccountLink),
-        body: <String, dynamic>{
-          'application-id': applicationId,
-          'application-secret': applicationSecret,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Accept': 'application/json'
+        },
+        body: jsonEncode(<String, dynamic>{
+          'application_id': applicationId,
+          'application_secret': applicationSecret,
           'type': type,
           'iban_number': ibanNumber,
           'beneficiary_name': beneficiaryName,
@@ -56,9 +44,9 @@ class UpdateAccountService {
           'business_address': address,
           'business_mobile': mobileNumber.toString(),
           'bank_id': bankId.toString(),
-          'business_documents': businessDocuments.toString(),
-          'bank_documents': bankDocuments.toString()
-        },
+          'business_documents': businessDocuments,
+          'bank_documents': bankDocuments
+        }),
       ).timeout(Duration(seconds: 5));
 
       if (response.statusCode == 200) {
@@ -67,8 +55,6 @@ class UpdateAccountService {
         updateResponse = updateAccountResponseFromJson(response.body);
         return updateResponse;
       } else {
-        print(
-            'Error with sCode: ${response.statusCode} and body: ${response.body}');
         MyAccountLocalData.networkConnectionState = true;
         MyAccountLocalData.dataSuccessState = false;
         return null;
